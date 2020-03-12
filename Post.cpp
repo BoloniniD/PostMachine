4a3265954b857d4c3a1f23bf8e5138b31039c162
BOLL <54375878+BoloniniD@users.noreply.github.com>
2020-03-12 14:35:24 +0300

    Update Post.cpp

--- Post.cpp
+++ Post.cpp
@@ -2,11 +2,31 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <stdexcept>
 
 using namespace std;
 
+pair<string, int> word(string s, int j) {
+    string r ;
+    int t = j;
+    for (size_t i = j; i <= s.size(); ++i) {
+        t = i;
+        if (t >= s.size()) {
+            break;
+        }
+        if (s[i] == ' ') {
+            ++t;
+            break;
+        } else {
+            r += s[i];
+        }
+    }
+    return make_pair(r, t);
+}
+
 int main() {
     vector<bool> line;
+    int curr = 0;
     vector<string> prog;
     ifstream inp_line, inp_prog;
     string name, str;
@@ -49,4 +69,115 @@ int main() {
         }
     }
     inp_line.close();
+    for (size_t i = 0; i < prog.size(); ++i) {
+        str = prog[i];
+        auto pa1 = word(str, 0);
+        if (curr < 0 && curr >= line.size()) {
+            cerr << "Machine's head is out of sequence!\nShutting down...\n";
+            break;
+        }
+        if (pa1.first == "V") {
+            auto pa2 = word(str, pa1.second);
+            try {
+                if (pa2.first == "") {
+                    int temp = i + 1;
+                    i = temp - 1;
+                } else {
+                    int temp = stoi(pa2.first);
+                    i = temp - 1;
+                }
+                if (line[curr]) {
+                    cerr << "Marking an already marked slot!\nShutting down...\n";
+                } else {
+                    line[curr] = true;
+                }
+            } catch (std::invalid_argument& e) {
+                cerr << "Expected integer or empty space in line " << i << ", found " << pa2.first << '\n';
+                break;
+            }
+        }
+        if (pa1.first == "X") {
+            auto pa2 = word(str, pa1.second);
+            try {
+                if (pa2.first == "") {
+                    int temp = i + 1;
+                    i = temp - 1;
+                } else {
+                    int temp = stoi(pa2.first);
+                    i = temp - 1;
+                }
+                if (!line[curr]) {
+                    cerr << "Cannot remove a mark from an unmarked slot!\nShutting down...\n";
+                } else {
+                    line[curr] = false;
+                }
+            } catch (std::invalid_argument& e) {
+                cerr << "Expected integer or empty space in line " << i << ", found " << pa2.first << '\n';
+                break;
+            }
+        }
+        if (pa1.first == "<") {
+            auto pa2 = word(str, pa1.second);
+            try {
+                if (pa2.first == "") {
+                    int temp = i + 1;
+                    i = temp - 1;
+                } else {
+                    int temp = stoi(pa2.first);
+                    i = temp - 1;
+                }
+                --curr;
+            } catch (std::invalid_argument& e) {
+                cerr << "Expected integer or empty space in line " << i << ", found " << pa2.first << '\n';
+                break;
+            }
+        }
+        if (pa1.first == ">") {
+            auto pa2 = word(str, pa1.second);
+            try {
+                if (pa2.first == "") {
+                    int temp = i + 1;
+                    i = temp - 1;
+                } else {
+                    int temp = stoi(pa2.first);
+                    i = temp - 1;
+                }
+                ++curr;
+            } catch (std::invalid_argument& e) {
+                cerr << "Expected integer or empty space in line " << i << ", found " << pa2.first << '\n';
+                break;
+            }
+        }
+        if (pa1.first == "?") {
+            auto pa2 = word(str, pa1.second);
+            auto pa3 = word(str, pa2.second);
+            if (line[curr]) {
+                try {
+                    auto temp = stoi(pa2.first);
+                    i = temp - 1;
+                } catch (std::invalid_argument& e) {
+                    cerr << "Expected integer or empty space in line " << i << ", found " << pa2.first << '\n';
+                    break;
+                }
+            } else {
+                try {
+                    auto temp = stoi(pa3.first);
+                    i = temp - 1;
+                } catch (std::invalid_argument& e) {
+                    cerr << "Expected integer or empty space in line " << i << ", found " << pa3.first << '\n';
+                    break;
+                }
+            }
+        }
+        if (pa1.first == "!") {
+            cout << "Found !, shutting down...\n";
+            break;
+        }
+    }
+    ofstream fout;
+    fout.open(name + " output.postline");
+    for (size_t i = 0; i < line.size(); ++i) {
+        fout << static_cast<int>(line[i]);
+    }
+    fout.close();
 }
